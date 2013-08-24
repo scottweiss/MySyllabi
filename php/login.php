8d9f0e843f98f904780c1bb9f65efca8a8587631
Scott Weiss <scott.a.weiss@gmail.com>
2013-08-24 14:47:39 -0400

    added titles to validation
    no one is reading this

--- php/login.php
+++ php/login.php
@@ -3,8 +3,8 @@
 <a href="register.php">Don't have an account?</a>
 
 <form action="php/loginScript.php" method="post">
-	<input class="loginInput" name="userName" class="mainSearch" placeholder="email or username" required>
-	<input class="loginInput" type="password" name="password" class="mainSearch" placeholder="password" required>
+	<input class="loginInput" name="userName" class="mainSearch" placeholder="email or username" title="If you dont have either please register!" required autofocus>
+	<input class="loginInput" type="password" name="password" class="mainSearch" placeholder="password" title="Forgot your password? See link below." required>
 	<input class="loginInput" type="submit" value="Login">
 </form>
 
